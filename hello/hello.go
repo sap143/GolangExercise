e9bb4cf39a39d05202ca002325e8f18e03283cc9
sap143 <salman007ansari786@gmail.com>
2022-09-25 19:24:39 +0530

    Added feature in greeting module to check is empty or not .

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Salman")
+	// message := greetings.Hello("Salman")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
